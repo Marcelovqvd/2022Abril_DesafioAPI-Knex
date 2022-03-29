45702fb612ee74448db3f6ed9b6008c8faf938f9
Marcelo <marcelovqvd@gmail.com>
2022-03-29 00:53:23 -0300

    wip

--- src/routes/Users.routes.js
+++ src/routes/Users.routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const knex = require('../database')
+
 const usersRoutes = express.Router()
 
-usersRoutes.post("/users", (request, response) => {
+usersRoutes.post('/users', (request, response) => {
   return response.send()
 });
 
-usersRoutes.get("/users", (request, response) => {
-  return response.status(200).json({msg: 'get'})
-  //return knex('user').then(results => response.json(results))
-});
+usersRoutes.get('/users', (request, response) => knex('user').then(results => response.json(results)));
 
-module.export = usersRoutes
+module.exports = usersRoutes
