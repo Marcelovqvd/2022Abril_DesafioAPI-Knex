ac844c23b30c1ac49bca7630dd939b5a972f7b5a
Marcelo <marcelovqvd@gmail.com>
2022-04-07 17:25:44 -0300

    fix host

--- knexfile.js
+++ knexfile.js
@@ -4,23 +4,23 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-    client: 'postgresql',
-    connection: {
-      host: 'database',
-      user: 'marcelo',
-      database: 'contele_db',
-      password: '123'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${__dirname}/src/database/migrations`,
-      disableMigrationsListValidation: true,
-    },
-    seeds: {
-      directory: `${__dirname}/src/database/seeds`
-    }
+  client: 'postgresql',
+  connection: {
+    host: 'localhost',
+    user: 'marcelo',
+    database: 'contele_db',
+    password: '123'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: `${__dirname}/src/database/migrations`,
+    disableMigrationsListValidation: true,
+  },
+  seeds: {
+    directory: `${__dirname}/src/database/seeds`
+  }
 }
